87ec60c0fe20f6c453f0c4d10c896dc7d9295d26
thewasmans <thewasmans@gmail.com>
2026-05-03 19:31:38 +0200

    Add ui interraction

--- scripts/managers/box_manager.gd
+++ scripts/managers/box_manager.gd
@@ -1,10 +1,23 @@
-class_name BoxManager
-extends Node
+class_name DemoManager
+extends Manager
 
-@export var box: Node3D
+@export var player_box: RigidBody3D
+@export var spawn_point_player: Node3D
 
-var _time: float
+func _ready() -> void:
+	spawn_point_player.visible = false
 
-func _process(delta: float) -> void:
-	_time += delta
-	box.position = Vector3.UP * sin(_time) * .25
+func _physics_process(delta: float) -> void:
+	var forward_dir = -player_box.global_transform.basis.z
+	var input_dir = Input.get_axis("action_backward", "action_forward")
+	var rotation_dir = Input.get_axis("action_turn_right", "action_turn_left")
+	
+	if input_dir != 0:
+		player_box.linear_velocity = forward_dir * input_dir * game_data.linear_speed
+	
+	if rotation_dir != 0:
+		player_box.rotate_y(rotation_dir * delta * game_data.rotation_speed)
+
+func respawn_player():
+	print("respawn")
+	player_box.global_position = spawn_point_player.global_position
--- scripts/managers/game_managers.gd
+++ scripts/managers/game_managers.gd
@@ -3,14 +3,15 @@ class_name GameManager
 
 signal initialized
 
-@export var game_data:GameData
+@export var game_data: GameData
 @export_category("Managers")
-@export var game_ui_manager:GameUIManager
+@export var game_ui_manager: GameUIManager
+@export var demo_manager: DemoManager
 
 var game_state:GameState
 var managers:Array[Manager]:
 	get:
-		return [game_ui_manager]
+		return [game_ui_manager, demo_manager]
 
 func initialize():
 	game_state = GameState.new(game_data)
